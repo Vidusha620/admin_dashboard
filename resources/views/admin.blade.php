f260d22e3cacd321a4c73b4ffa3ba00825f37728
Vidusha620 <hasindividusha@gmail.com>
2024-09-09 19:16:59 +0530

    adding Scripts

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -12,5 +12,11 @@
 
     </head>
     <body>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/scripts.js)}}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/datatables-simple-demo.js')}}"></script>
+
 </body>
 </html>
