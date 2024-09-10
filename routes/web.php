0bb5441cd6b53df566754766bd5291a23ec7fad1
Vidusha620 <hasindividusha@gmail.com>
2024-09-10 15:32:32 +0530

    Updating

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('admin');
-});
+})->name('admin.home');
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
